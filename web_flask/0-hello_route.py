9b7ca964134b97056e3d858ca58b8ed06ab36617
humphreyotieno1 <humphreyotieno04@gmail.com>
2024-01-28 22:00:17 +0000

    Starting a Flask web app

---
+++ web_flask/0-hello_route.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""Bash script to start flask web server"""
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/", strict_slashes=False)
+def hello():
+    """home section"""
+    return "Hello HBNB!"
+
+if __name__ == '__main__':
+    app.run(debug=True, port=5000, host='0.0.0.0')
