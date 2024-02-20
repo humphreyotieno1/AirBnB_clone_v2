d18aaa71eea1be2e38d6b4776b6bd1a85a7d360b
humphreyotieno1 <humphreyotieno04@gmail.com>
2024-02-20 19:43:38 +0000

    Updates

--- web_flask/6-number_odd_or_even.py
+++ web_flask/6-number_odd_or_even.py
@@ -1,49 +1,55 @@
 #!/usr/bin/python3
-"""script to start flask web server"""
+"""Simple Flask web application"""
 from flask import Flask, render_template
+app = Flask('web_flask')
+app.url_map.strict_slashes = False
 
-app = Flask(__name__)
 
-@app.route("/", strict_slashes=False)
-def hello():
-    """home section"""
-    return "Hello HBNB!"
+@app.route('/')
+def hello_route1():
+    """Return 'Hello HBNB!'"""
+    return 'Hello HBNB!'
 
-@app.route("/hbnb", strict_slashes=False)
-def hbnb():
-    """hbnb section"""
-    return "HBNB"
 
-@app.route("/c/<text>/", strict_slashes=False)
-@app.route("/c", strict_slashes=False)
-def myvar(text="is cool"):
-    """the c section"""
-    return "C {}".format(text.replace("_", " "))
+@app.route('/hbnb')
+def hello_route2():
+    """Return 'HBNB'"""
+    return 'HBNB'
 
-@app.route("/python/<text>/", strict_slashes=False)
-@app.route("/python", strict_slashes=False)
-def myPythonText(text="is cool"):
-    """python section"""
-    return "Python {}".format(text.replace("_", " "))
 
-@app.route("/number/<int:n>", strict_slashes=False)
-def number(n):
-    """number section"""
-    return "{} is a number".format(n)
+@app.route('/c/<text>')
+def hello_route3(text):
+    """Return 'C ' followed by text from html request"""
+    return 'C {}'.format(text.replace('_', ' '))
 
-@app.route("/number_template/<int:n>", strict_slashes=False)
-def number_two(n):
-    """number_template section"""
-    return render_template('5-number.html', content=n)
 
-@app.route("/number_odd_or_even/<int:n>", strict_slashes=False)
-def number_odd_or_even(n):
-    """odd or even section"""
-    if n % 2 == 0:
-        p = 'even'
-    else:
-        p = 'odd'
-    return render_template('6-number_odd_or_even.html', content=n, even_odd=p)
-  
-if __name__ == '__main__':
-    app.run(debug=True, port=5000, host='0.0.0.0')
+@app.route('/python/<text>')
+@app.route('/python/', defaults={'text': 'is cool'})
+def hello_route4(text):
+    """Return 'Python ' followed by text from html request with
+    default text 'is cool'"""
+    return 'Python {}'.format(text.replace('_', ' '))
+
+
+@app.route('/number/<int:n>')
+def hello_route5(n):
+    """Return last part of html request formatted as a number if
+    it can be converted to an int"""
+    return '{:d} is a number'.format(n)
+
+
+@app.route('/number_template/<int:n>')
+def hello_route6(n):
+    """Return html template containing the number `n`"""
+    return render_template('5-number.html', n=n)
+
+
+@app.route('/number_odd_or_even/<int:n>')
+def hello_route7(n):
+    """Return rendered html containing logic that determines whether
+    `n` is even or odd and displays the result in an <h1> tag"""
+    return render_template('6-number_odd_or_even.html', n=n)
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
